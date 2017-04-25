c96f7a9efabe2aced4a5b131b442f40f04017ead
root <root@ygavhane-dataxu-com2.mylabserver.com>
2017-04-25 22:43:27 +0000

    Study for Qualys

---
+++ cookbooks/test-qualys-apache/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: test-qualys-apache
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+package "httpd" 
+
+#service "httpd" do
+#	action [:enable, :start]
+#end
+
+#template "/var/www/html/index.html"  do
+#	source "index.html.erb"
+#end
+
+
--- cookbooks/test/recipes/default.rb
+++ cookbooks/test/recipes/default.rb
@@ -13,3 +13,7 @@ end
 service 'httpd' do
 	action [ :start ,:enable ]
 end
+
+template "/var/www/html/index.html" do
+	source "index.html.erb"
+end
